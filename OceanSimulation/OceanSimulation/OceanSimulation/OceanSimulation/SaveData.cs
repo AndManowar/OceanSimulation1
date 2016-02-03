c57a391b78bca57911c2cfb8c14991e0b431c449
AndManowar <FranceOlolo@yandex.ru>
2016-02-03 13:47:02 +0200

    OceanSimulation

---
+++ OceanSimulation/OceanSimulation/OceanSimulation/OceanSimulation/SaveData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace OceanSimulation
+{
+    class SaveData
+    {
+        public static void saving(BaseObject Object)
+        {
+
+            using (FileStream fs = new FileStream(@"Saves\1.txt", FileMode.Create, FileAccess.Write))
+            {
+                BinaryFormatter serializer = new BinaryFormatter();
+                serializer.Serialize(fs, Object);
+
+
+            }
+        }//Save
+        public static List<BaseObject> Loading()
+        {
+            List<BaseObject> lst;
+
+            using (FileStream fs = File.Open(@"Saves\1.txt", FileMode.Open))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                lst = (List<BaseObject>)formatter.Deserialize(fs);
+                return lst;
+            }
+        }
+    }
+}
---
+++ OceanSimulation/OceanSimulation/OceanSimulation/OceanSimulation/SimpleFish.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OceanSimulation
+{
+   
+   public class SimpleFish : BaseObject
+    {
+        public SimpleFish() { }
+        public SimpleFish(Texture2D Simplefish, Vector2 position, Vector2 spd, int lifeTime, int repro)
+        {
+            baseObject = Simplefish;
+            MinDepth = 0;
+            MaxDepth = 200;
+            Position.X = position.X;
+            Position.Y = position.Y;
+            speed.X = spd.X;
+            speed.Y = spd.Y;
+            LifeTime = lifeTime;       
+            ReproTime = repro;
+            meet = false;
+            sc = true;
+        }
+   
+    }
+}
